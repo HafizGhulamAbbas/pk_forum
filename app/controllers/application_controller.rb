cd7d2c6d59bab372265a1ec30db2e431fe1c1370
HafizGhulamAbbas <hgabbas1122@gmail.com>
2022-01-02 23:40:22 +0500

    Fix like/unlike redirection issue for unauthorized-person

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,7 +13,7 @@ class ApplicationController < ActionController::Base
     end
 
     def authorized
-        # redirect_to '/welcome' unless logged_in?
+        redirect_to login_path unless logged_in?
     end
 
     def set_tags
