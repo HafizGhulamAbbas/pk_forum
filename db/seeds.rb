a19cbe4235277f9a0e7ed08f1f3b225221f7ab46
HafizGhulamAbbas <hgabbas1122@gmail.com>
2022-01-02 16:33:54 +0500

    Run seed on production

--- db/seeds.rb
+++ db/seeds.rb
@@ -133,4 +133,4 @@ end
   )
 end
 
-AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.production?
